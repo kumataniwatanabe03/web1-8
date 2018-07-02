6e1d516a95eca671a9fedfa7e1255da738ba7e26
Unknown <0104kumakiri@gmail.com>
2018-07-02 18:08:46 +0900

    エフェクトがすごいことになりましたが、修正できなかったのでそのままです。

---
+++ Igaguri/Assets/IgaguriGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgaguriGenerator : MonoBehaviour {
+
+    public GameObject igaguriPreab;
+	
+	void Update () {
+        if (Input.GetMouseButtonDown(0)) {
+            GameObject igaguri =
+                Instantiate(igaguriPreab) as GameObject;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 worldDir = ray.direction;
+            igaguri.GetComponent<IgaguriController>().Shoot(
+                worldDir.normalized * 2000);
+        }
+	}
+}
